3d5813b1d3caf16853a876b94372a215deb6e2f8
remimarenco <remimarenco@gmail.com>
2014-01-15 14:46:15 +0100

    Initial commit

---
+++ PousseDeBambin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PousseDeBambin.MvcApplication" Language="C#" %>
---
+++ PousseDeBambin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PousseDeBambin.Startup))]
+namespace PousseDeBambin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
